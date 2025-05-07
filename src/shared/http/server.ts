15120964f34d2aba601c26b8d886dd8948d8bca4
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-05-07 07:57:24 -0300

    feat: lint, config and database

---
+++ src/shared/http/server.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import { routes } from './routes';
+import cors from 'cors';
+import 'reflect-metadata';
+
+import 'express-async-errors';
+import ErrorHandleMiddleware from '@shared/middlewares/ErrorHandleMiddleware';
+import { AppDataSource } from '@shared/typeorm/data-source';
+
+AppDataSource.initialize().then(async () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(routes);
+
+  app.use(ErrorHandleMiddleware.handleError);
+
+  console.log('Connected to database');
+
+  app.listen(3333, () => console.log('Server is running on port 3333'));
+
+}).catch((err) => {
+  console.error('Error during Data Source initialization', err);
+});
+
